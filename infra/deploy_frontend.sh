91ed1c290c6d093b875d86e1f8e3d91409f0b173
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2026-03-17 14:47:29 -0300

    adjust files of deploy

--- infra/deploy_frontend.sh
+++
@@ -1,3 +0,0 @@
-docker compose down
-docker rmi daniel101/${SERVICE_NAME}:"${IMAGE_VERSION}"
-docker compose up -d
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,26 @@ cd ./infra
 sed -i.bak s#\\\${SERVICE_NAME}#$SERVICE_NAME#g infra/docker-compose.yml
 sed -i.bak s#\\\${IMAGE_VERSION}#$IMAGE_VERSION#g infra/docker-compose.yml
 
-chmod +x ./configure-apache.sh
-chmod +x ./deploy_frontend.sh
-./deploy_frontend.sh
+docker compose down
+docker rmi daniel101/${SERVICE_NAME}:"${IMAGE_VERSION}"
+docker compose up -d
+
 sleep 5
-./configure-apache.sh
+
+# remove cache files
+rm -rf /etc/apache2/sites-enabled/norte4j.com
+rm -rf /etc/apache2/sites-available/norte4j.com
+
+# apache configuration
+cp ./norte4j.com.conf /etc/apache2/sites-available/
+a2ensite norte4j.com.conf
+systemctl restart apache2
+
+## certbot configuration
+
+#certbot --apache \
+#          -d norte4j.com \
+#          --agree-tos \
+#          --non-interactive \
+#          --register-unsafely-without-email \
+#          --redirect
