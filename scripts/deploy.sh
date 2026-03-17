e576dfabba0b7608bf61369f446897b4d6568f71
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2026-03-17 15:12:05 -0300

    adjust files of deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,9 +13,9 @@ rm -rf /etc/apache2/sites-enabled/norte4j.com
 rm -rf /etc/apache2/sites-available/norte4j.com
 
 # apache configuration
-sudo -s cp ./norte4j.com.conf /etc/apache2/sites-available/
+cp ./norte4j.com.conf /etc/apache2/sites-available/
 a2ensite norte4j.com.conf
-sudo -s systemctl restart apache2
+systemctl restart apache2
 
 ## certbot configuration
 
